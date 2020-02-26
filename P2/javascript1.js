5c024fdc5096d7073752940d3999afc341221fb8
Raul Mesonero Fernandez <rmesoner@f-l3207-pc45.aulas.etsit.urjc.es>
2020-02-26 19:51:56 +0100

    Calculadora terminada (solo faltan retoques)

--- P2/javascript1.js
+++ P2/javascript1.js
@@ -1,5 +1,9 @@
 console.log("REFFERENZ'S XTREME CALCULATOR");
 
+var operandoa;
+var operandob;
+var operacion;
+
 function init (){
   var reset = document.getElementById('reset');
   var dividir = document.getElementById('dividir');
@@ -28,12 +32,93 @@ function init (){
 }
 
 uno.onclick = function(e){
-  resultado.TextContent = resultado.TextContent + "1";
+  resultado.textContent = resultado.textContent + "1";
+}
+dos.onclick = function(e){
+  resultado.textContent = resultado.textContent + "2";
+}
+tres.onclick = function(e){
+  resultado.textContent = resultado.textContent + "3";
+}
+cuatro.onclick = function(e){
+  resultado.textContent = resultado.textContent + "4";
+}
+cinco.onclick = function(e){
+  resultado.textContent = resultado.textContent + "5";
+}
+seis.onclick = function(e){
+  resultado.textContent = resultado.textContent + "6";
+}
+siete.onclick = function(e){
+  resultado.textContent = resultado.textContent + "7";
+}
+ocho.onclick = function(e){
+  resultado.textContent = resultado.textContent + "8";
+}
+nueve.onclick = function(e){
+  resultado.textContent = resultado.textContent + "9";
+}
+cero.onclick = function(e){
+  resultado.textContent = resultado.textContent + "0";
+}
+reset.onclick = function(e){
+  resetear();
+}
+sumar.onclick = function(e){
+  operandoa = resultado.textContent;
+  operacion = "+";
+  limpiar();
+}
+restar.onclick = function(e){
+  operandoa = resultado.textContent;
+  operacion = "-";
+  limpiar();
+}
+multiplicar.onclick = function(e){
+  operandoa = resultado.textContent;
+  operacion = "*";
+  limpiar();
+}
+dividir.onclick = function(e){
+  operandoa = resultado.textContent;
+  operacion = "/";
+  limpiar();
+}
+igual.onclick = function (e) {
+  operandob = resultado.textContent;
+  resolver();
+}
+borrar.onclick = function (e) {
+  resultado.textContent = "0";
 }
 
-reset.onclick = () => {
-  console.log("Esto es resetear")
+function limpiar() {
+  resultado.textContent = "";
 }
-dividir.onclick = () => {
-  console.log("Esto es dividir")
+
+function resetear () {
+  operandoa = 0;
+  operandob = 0;
+  operacion = "";
+  resultado.textContent = "";
+}
+
+function resolver(){
+  var res = 0;
+  switch (operacion) {
+    case "+":
+      res = parseFloat(operandoa) + parseFloat(operandob);
+      break;
+    case "-":
+      res = parseFloat(operandoa) - parseFloat(operandob);
+      break;
+    case "*":
+      res = parseFloat(operandoa) * parseFloat(operandob);
+      break;
+    case "/":
+      res = parseFloat(operandoa) / parseFloat(operandob);
+      break;
+  }
+  resetear();
+  resultado.textContent = res;
 }
